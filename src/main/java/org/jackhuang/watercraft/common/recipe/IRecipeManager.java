a5992c866733640b720e9408d81aaffb2e573f68
huanghongxun <huanghongxun2008@126.com>
2014-11-19 20:01:07 +0800

    add licenses.

--- src/main/java/org/jackhuang/watercraft/common/recipe/IRecipeManager.java
+++ src/main/java/org/jackhuang/watercraft/common/recipe/IRecipeManager.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Huang Yuhui, 2014
+ * 
+ * "WaterCraft" is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license located in
+ * http://www.mod-buildcraft.com/MMPL-1.0.txt
+ */
+
 package org.jackhuang.watercraft.common.recipe;
 
 import java.util.Map;
--- src/main/java/org/jackhuang/watercraft/common/recipe/MyRecipeOutput.java
+++ src/main/java/org/jackhuang/watercraft/common/recipe/MyRecipeOutput.java
@@ -1,7 +1,5 @@
 package org.jackhuang.watercraft.common.recipe;
 
-import ic2.api.recipe.RecipeOutput;
-
 import java.util.Arrays;
 import java.util.List;
 
