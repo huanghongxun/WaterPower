90d544f259e821700686e4dbbe61deac5e8ecc05
huanghongxun <huanghongxun2008@126.com>
2015-08-04 00:10:55 +0800

    Now Turbines can change its output energy unit

--- src/main/java/org/jackhuang/watercraft/common/block/turbines/GuiTurbine.java
+++ src/main/java/org/jackhuang/watercraft/common/block/turbines/GuiTurbine.java
@@ -1,5 +1,7 @@
 package org.jackhuang.watercraft.common.block.turbines;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
@@ -8,6 +10,9 @@ import net.minecraft.util.StatCollector;
 
 import org.jackhuang.watercraft.Reference;
 import org.jackhuang.watercraft.client.gui.ContainerRotor;
+import org.jackhuang.watercraft.common.EnergyType;
+import org.jackhuang.watercraft.common.network.MessagePacketHandler;
+import org.jackhuang.watercraft.common.network.PacketUnitChanged;
 import org.lwjgl.opengl.GL11;
 
 import cpw.mods.fml.relauncher.Side;
@@ -16,12 +21,25 @@ import cpw.mods.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class GuiTurbine extends GuiContainer {
 	private TileEntityTurbine gen;
+	
+    private GuiButton btnEnergyType;
 
 	public GuiTurbine(EntityPlayer player, TileEntityTurbine gen) {
 		super(new ContainerRotor(player, gen));
 		this.gen = gen;
 		allowUserInput = false;
 	}
+
+    @Override
+    public void initGui() {
+        super.initGui();
+
+        int l = (width - xSize) / 2;
+        int i1 = (height - ySize) / 2;
+        btnEnergyType = new GuiButton(1, l + 100, i1 + 24, 30, 20,
+                gen.energyType.name());
+        this.buttonList.add(btnEnergyType);
+    }
 
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float f, int i, int j) {
@@ -42,4 +60,22 @@ public class GuiTurbine extends GuiContainer {
 		fontRendererObj.drawString(StatCollector.translateToLocal("cptwtrml.watermill.OUTPUT") + ": " + gen.getOfferedEnergy() + "EU/t", 8,
 				50, 0x404040);
 	}
+
+    @Override
+    protected void actionPerformed(GuiButton p_146284_1_) {
+        super.actionPerformed(p_146284_1_);
+
+        switch (p_146284_1_.id) {
+        case 1:
+            gen.energyType = EnergyType.values()[(gen.energyType.ordinal() + 1)
+                    % EnergyType.values().length];
+            btnEnergyType.displayString = gen.energyType.name();
+            MessagePacketHandler.INSTANCE
+                    .sendToServer(new PacketUnitChanged(
+                            Minecraft.getMinecraft().thePlayer.worldObj.provider.dimensionId,
+                            gen.xCoord, gen.yCoord, gen.zCoord, gen.energyType
+                                    .ordinal()));
+            break;
+        }
+    }
 }
